383cd154a3bf3c6458a3daac59b2368d17f73a0b
Allison Espinoza <alleespi@espol.edu.ec>
2018-11-08 20:41:26 -0500

    Crear la interfaz CalcularPrecio

---
+++ src/Postres_bien/CalcularPrecio.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Postres_bien;
+
+/**
+ *
+ * @author Administrador
+ */
+public interface CalcularPrecio {
+      public double calcularPrecioFinal();
+
+    }
